2e718cdd14b60699a3cd6485efce7e9fed10d3e0
Janis <janis.wanger@novu.ch>
2021-12-06 21:24:23 +0100

    added linkresolver

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 export { default as useId, useIdMixin } from './common/useId/index.js'
 export { useDispatchStoreActionMixin } from './vue/useDispatchStoreAction/index.js'
 export * from './vue/popover/index.js'
+export * from './vue/linkResolver/index.js'
---
+++ vue/linkResolver/index.js
@@ -0,0 +1,59 @@
+
+import Vue from 'vue'
+
+
+// https://vuejs.org/v2/guide/custom-directive.html
+Vue.directive('linkresolver', {
+  // Called only once, when the directive is first bound to the element. This is where you can do one-time setup work.
+  bind(el,  binding, vnode) {
+    const navigate = event => {
+      const href = event.currentTarget.getAttribute('href')
+      //Seperate external and internal links
+      if (href && href[0] === '/') {
+        event.preventDefault()
+        event.metaKey
+        ? window.open(href, '_blank', 'noopener')
+        : vnode.context.$router.push(href)
+      }
+    }
+    
+    let links = el.getElementsByTagName('a')
+
+    const addListeners = links => {
+      for (let i = 0; i < links.length; i++) {
+        const target = links[i].getAttribute('target')
+
+        // For improved security `rel="noopener"` will be added automatically if target is `_blank`
+        // https://github.com/mathiasbynens/rel-noopener/
+        if (target && target === '_blank') {
+          const rel = links[i].getAttribute('rel')
+          const attributes = rel ? (rel + ' noopener') : 'noopener'
+          links[i].setAttribute('rel', attributes)
+        }
+        links[i].addEventListener('click', navigate, false)
+      }
+    }
+
+    const removeListeners = links => {
+      for (let i = 0; i < links.length; i++) {
+        links[i].removeEventListener('click', navigate, false)
+      }
+      links = []
+    }
+
+    addListeners(links)
+
+    el.$componentUpdated = () => {
+      removeListeners(links)
+      Vue.nextTick(() => addListeners(links))
+    }
+
+    el.$destroy = () => el.removeEventListener('click', removeListeners(links))
+  },
+
+  // Called after the containing component’s VNode and the VNodes of its children have updated.
+  componentUpdated: el => el.$componentUpdated(),
+
+  // Called only once, when the directive is unbound from the element.
+  unbind: el => el.$destroy()
+})
